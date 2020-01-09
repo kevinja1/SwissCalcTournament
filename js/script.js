917b370748971c38419b39ee306c2bb1bf061f8d
mgovilla <mgovilla@gmail.com>
2020-01-09 16:37:37 -0500

    Fix CORS issue

--- js/script.js
+++ js/script.js
@@ -57,33 +57,31 @@ function newElement() {
 }
 
 username = "RedLamp3";
-apikey = "UT8quogWEIJV5BAYIXZWSNbQXLHtdyhjBta1ra6E";
+apikey = '"5q3BnUgRfZZiRHVwufh40ysKU4xys1u3hYoM8AGT"';
 
 function createTournament() {
-    url = 'https://' + username + ':' + apikey + '@api.challonge.com/v1/tournaments.json';
-    console.log(url);
-
+    url = 'https://api.challonge.com/v1/tournaments.json';
     httpPostAsync(url, getResponse);
 
 
 }
 
 function getResponse(responseText) {
-    console.log(responseText);
+    console.log(responseText.tournament.url);
 }
 
 function httpPostAsync(url, callback) {
     
-    let params = JSON.stringify({ "tournament": { "name": "BC1920", "tournament_type": "swiss", "game_name": "Calculus" }});
-    console.log(params);
+    data = {"api_key": apikey, "tournament": { "name": "BC1920", "tournament_type": "swiss", "game_name": "Calculus" }};
+    var params = JSON.stringify(data);
     var xhr = new XMLHttpRequest();
 
     xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(params);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 302) {
             callback(JSON.parse(xhr.responseText));
         }
     };
-    xhr.send(params);
+    
 }
